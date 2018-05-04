0fdb0cc5069b51fcb5638f47ccc646307fa0594e
GwinSalbor <yaroslav.x2@gmail.com>
2018-05-04 18:27:49 +0300

    first commit

---
+++ Face.py
@@ -0,0 +1,55 @@
+import os
+import dlib
+import urllib.request
+from PIL import Image
+from skimage import io
+from scipy.spatial import distance
+
+
+sp = dlib.shape_predictor('shape_predictor_68_face_landmarks.dat')
+facerec = dlib.face_recognition_model_v1('dlib_face_recognition_resnet_model_v1.dat')
+detector = dlib.get_frontal_face_detector()
+img = io.imread('im.jpg')
+
+win1 = dlib.image_window()
+win1.clear_overlay()
+win1.set_image(img)
+dets = detector(img, 1)
+for k, d in enumerate(dets):
+    print("Detection {}: Left: {} Top: {} Right: {} Bottom: {}".format(
+        k, d.left(), d.top(), d.right(), d.bottom()))
+
+    shape = sp(img, d)
+    win1.clear_overlay()
+    win1.add_overlay(d)
+    win1.add_overlay(shape)
+face_descriptor1 = facerec.compute_face_descriptor(img, shape)
+print(face_descriptor1)
+
+directory = 'C:\\Users\\Yaroslav\\Desktop\\Foto'
+files = os.listdir(directory)
+#Фильтруем список
+images = filter(lambda x: x.endswith('.jpg'), files)
+
+for i in images:
+    img = io.imread(directory + '\\' + i)
+    win2 = dlib.image_window()
+    win2.clear_overlay()
+    win2.set_image(img)
+    dets_webcam = detector(img, 1)
+    for k, d in enumerate(dets_webcam):
+        print("Detection {}: Left: {} Top: {} Right: {} Bottom: {}".format(
+            k, d.left(), d.top(), d.right(), d.bottom()))
+        shape = sp(img, d)
+        win2.clear_overlay()
+        win2.add_overlay(d)
+        win2.add_overlay(shape)
+    face_descriptor2 = facerec.compute_face_descriptor(img, shape)
+    a = distance.euclidean(face_descriptor1, face_descriptor2)
+    wait = input("PRESS ENTER TO CONTINUE.")
+
+    a = distance.euclidean(face_descriptor1, face_descriptor2)
+    print(a)
+    if( a < 0.55):
+        image = Image.open("C:\\Users\\Yaroslav\\Desktop\\Foto\\"+ i)
+        image.save("C:\\Users\\Yaroslav\\Desktop\\Foto\\out\\" + i, "JPEG")
